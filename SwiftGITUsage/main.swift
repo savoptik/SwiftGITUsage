c931cba138d157cff5cfc604c9b37f5728e01455
savoptik <sav70792@gmail.com>
2020-03-21 14:38:57 +0300

    Initial Commit

---
+++ SwiftGITUsage/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftGITUsage
+//
+//  Created by Артём Семёнов on 21.03.2020.
+//  Copyright © 2020 Артём Семёнов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
